549dfe0e01ca1da01b88f796321db6bea8617979
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-05 14:16:13 +0200

    add data() return 'imgSRC' for img tag

--- main.js
+++ main.js
@@ -2,18 +2,9 @@ const appHello = Vue.createApp({
     data(){
         return {
             hello: 'Ciao! Questa è la prima cosa che ho creato con Vue.js',
+            imgSRC: 'https://picsum.photos/500/300',
         }
     }
-    
-});
-
-// const appImg = Vue.createApp({
-//     data(){
-//         return {
-//         img: 'https://picsum.photos/200/300',
-//         }
-//     }
-// })
+})
 
 appHello.mount("#appHello");
-// appImg.mount("#appImg");
